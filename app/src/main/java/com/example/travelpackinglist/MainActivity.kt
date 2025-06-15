176b52cf4d51e51c8b0092d2a2546b0ad19ff41f
NicoleOnkel <ST10497522@rcconnect.edu.za>
2025-06-15 15:27:16 +0200

    add

--- app/src/main/java/com/example/travelpackinglist/MainActivity.kt
+++ app/src/main/java/com/example/travelpackinglist/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.travelpackinglist
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,18 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val additems = findViewById<Button>(R.id.additem) // add items button
+
+
+
+
+        additems.setOnClickListener {
+
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+
+        }
+
     }
 }
